4f97aef6394c5e3e4b7910e5591eda10b28e0048
Nate Charbonneau <nathaniel.charbonneau@gmail.com>
2026-06-26 14:06:49 -0400

    first commit

---
+++ src/main.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+const Io = std.Io;
+
+const tuey = @import("tuey");
+
+// pub fn main(init: std.process.Init) !void {
+pub fn main() !void {
+    std.debug.print("Hello World\n", .{});
+
+}
+
+
+
+
